7111b430d4aae1f049670c1e97b434b8c080516f
jimerictibayan2012-netflix <90145084+jimerictibayan2012-netflix@users.noreply.github.com>
2021-09-10 00:11:21 +0800

    Update config.py

--- config.py
+++ config.py
@@ -8,14 +8,12 @@ API_SECRET = os.environ.get("API_SECRET")
 MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
 MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
-RISK = os.environ.get("RISK")
-
-TEST = os.environ.get("TEST") #Trading Settings (0=Live Trading), (1=Test Trading), (2=Fixed Account Balance)
+RISK = 0.01
 
 #TEST = 0  #Live Trading
 
-#TEST = 1  #Test Orders Only
+TEST = 1  #Test Orders Only
 
 #TEST = 2 #ACCOUNT Balance will have a fixed Amount. 
 
-TEST_ACCOUNT = os.environ.get("TEST_ACCOUNT")
+TEST_ACCOUNT = 1000
